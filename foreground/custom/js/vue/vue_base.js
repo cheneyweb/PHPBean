0a17e9e9befee75090fd906b0428304ba08e4559
cheneyweb <457299596@qq.com>
2017-01-11 11:02:25 +0800

    PHPBean 3.0

--- foreground/custom/js/vue/vue_base.js
+++ foreground/custom/js/vue/vue_base.js
@@ -1,6 +1,30 @@
 var VueBase = Vue.extend({
     methods: {
             /**
+             * 通用初始化
+             * 设置query.bean和query.id,以便查询
+             */
+            initQuery: function(id){
+                var vm = this
+                vm.query.bean = vm.bean
+                vm.query.id = id
+            },
+            initEntity: function(){
+                var vm = this
+                vm.entity.bean = vm.bean
+                vm.entity.id = null
+            },
+            /**
+             * 通用JSON数据接收
+             * 将返回的JSON字符串转化成JSON对象，并设置vm的respMsg
+             */
+            receiveData: function(sucResp){
+                var vm = this
+                var respObj = ($.parseJSON($.trim(sucResp.data)))
+                vm.respMsg = respObj.respMsg
+                return respObj;
+            },
+            /**
              * 通用查询操作方法集
              * beforeQuery和afterQuery提供重写拓展
              */
@@ -8,22 +32,21 @@ var VueBase = Vue.extend({
             afterQuery: function(respObj){},
             queryLoad: function() {
                 var vm = this
-                // 设置query.bean和query.id,以便查询
-                vm.query.bean = vm.bean
-                vm.query.id = null
-                // =====查询前置操作=====
+                // =====1,查询初始化操作=====
+                vm.initQuery(null)
+                // =====2,查询前置操作=====
                 vm.beforeQuery()
 
                 vm.$http.post(vm.queryUrl, vm.query)
                     .then((sucResp) => {
-                        var respObj = ($.parseJSON($.trim(sucResp.data)))
-                        vm.respMsg = respObj.respMsg
+                        // =====3,接收数据操作=====
+                        var respObj = vm.receiveData(sucResp)
                         if(vm.respMsg == 'Y'){
                             // 获取返回数据列表
                             vm.entityList = respObj.result
                             // 获取返回查询条件
                             vm.query =  respObj.query
-                            // =====查询后置操作=====
+                            // =====4,查询后置操作=====
                             vm.afterQuery(respObj)
                         }
                     }, (errResp) => {
@@ -31,36 +54,30 @@ var VueBase = Vue.extend({
                     });
             },
             /**
-             * [新增前置操作（可重写）]
-             */
-            beforeAdd: function(){},
-            /**
-             * [新增后置操作（可重写）]
-             */
-            afterAdd: function(){},
-            /**
-             * [通用新增操作]
+             * [通用新增操作集]
+             * beforeAdd和afterAdd提供重写拓展
              * @param modalId [弹窗ID]
              */
+            beforeAdd: function(){},
+            afterAdd: function(){},
             add: function(modalId) {
                 var vm = this
-                // 设置entity.bean和entity.id,以便新增
-                vm.entity.bean = vm.bean
-                vm.entity.id = null
-                // =====新增前置操作=====
+                // =====1,实体初始化操作=====
+                vm.initEntity()
+                // =====2,新增前置操作=====
                 vm.beforeAdd()
 
                 vm.$http.post(vm.addUrl, vm.entity)
                     .then((sucResp) => {
-                        var respObj = ($.parseJSON($.trim(sucResp.data)))
-                        vm.respMsg = respObj.respMsg
+                        // =====3,接收数据操作=====
+                        var respObj = vm.receiveData(sucResp)
                         if (vm.respMsg == 'Y') {
                             // 隐藏弹窗，重新查询
                             if(modalId != null){
                                 $('#' + modalId).modal('hide')
                                 vm.queryLoad()
                             }
-                            // =====新增后置操作=====
+                            // =====4,新增后置操作=====
                             vm.afterAdd()
                         }
                     }, (errResp) => {
@@ -77,30 +94,30 @@ var VueBase = Vue.extend({
             afterUpdateModal: function(){},
             /**
              * [通用更新弹窗]
+             * beforeUpdateModal和afterUpdateModal提供重写拓展
              * @param modalId [弹窗ID]
              * @param id [更新项ID]
              */
             updateModal: function(modalId,id) {
                 var vm = this
-                // 设置查询的ID
-                vm.query.bean = vm.bean
-                vm.query.id = id
-                // =====更新弹窗前置操作=====
+                // =====1,查询初始化操作=====
+                vm.initQuery(id)
+                // =====2,更新弹窗前置操作=====
                 vm.beforeUpdateModal()
 
                 vm.$http.post(vm.updateViewUrl, vm.query)
                     .then((sucResp) => {
-                        var respObj = ($.parseJSON($.trim(sucResp.data)))
-                        vm.respMsg = respObj.respMsg
+                        // =====3,接收数据操作=====
+                        var respObj = vm.receiveData(sucResp)
                         if(vm.respMsg == 'Y'){
                             // 获取详情数据
-                            vm.entity = $.parseJSON($.trim(sucResp.data))
+                            vm.entity = respObj
                             // 更换提交按钮
                             $('#updateBtn').show()
                             $('#addBtn').hide()
                             // 修改主题文字
                             $('#addModalLabel').html($('#addModalLabel').html().replace('新增','编辑'))
-                            // =====更新弹窗后置操作=====
+                            // =====4,更新弹窗后置操作=====
                             vm.afterUpdateModal()
                             // 显示更新弹窗
                             $('#' + modalId).modal()
@@ -110,34 +127,30 @@ var VueBase = Vue.extend({
                     });
             },
             /**
-             * [更新前置操作]
-             */
-            beforeUpdate: function(){},
-            /**
-             * [更新后置操作]
-             */
-            afterUpdate: function(){},
-            /**
-             * [通用更新操作]
+             * [通用更新操作集]
+             * beforeUpdate和afterUpdate提供重写拓展
              * @param modalId [弹窗ID]
              */
+            beforeUpdate: function(){},
+            afterUpdate: function(){},
             update: function(modalId) {
                 var vm = this
+                // =====1,实体初始化操作=====
                 vm.entity.bean = vm.bean
-                // =====更新前置操作=====
+                // =====2,更新前置操作=====
                 vm.beforeUpdate()
 
                 vm.$http.post(vm.updateUrl, vm.entity)
                     .then((sucResp) => {
-                        var respObj = ($.parseJSON($.trim(sucResp.data)))
-                        vm.respMsg = respObj.respMsg
+                        // =====3,接收数据操作=====
+                        var respObj = vm.receiveData(sucResp)
                         if(vm.respMsg == 'Y'){
                             // 隐藏弹窗，重新查询
                             if(modalId != null){
                                 $('#' + modalId).modal('hide');
                                 vm.queryLoad()
                             }
-                            // =====更新后置操作=====
+                            // =====4,更新后置操作=====
                             vm.afterUpdate()
                         }
                     }, (errResp) => {
@@ -151,8 +164,7 @@ var VueBase = Vue.extend({
                 var vm = this
                 vm.$http.post(vm.deleteUrl, {"ids":vm.ids})
                     .then((sucResp) => {
-                        var respObj = ($.parseJSON($.trim(sucResp.data)))
-                        vm.respMsg = respObj.respMsg
+                        var respObj = vm.receiveData(sucResp)
                         if(vm.respMsg == 'Y'){
                             // 重新查询
                             vm.queryLoad()
@@ -221,32 +233,26 @@ var VueBase = Vue.extend({
                 })
             },
             /**
-             * [显示弹窗前置操作（可重写）]
-             */
-            beforeShowModal: function(){},
-            /**
-             * [显示弹窗后置操作（可重写）]
-             */
-            afterShowModal: function(){},
-            /**
-             * [通用显示弹窗]
+             * [通用显示弹窗操作集]
+             * beforeShowModal和afterShowModal提供重写拓展
              * @param modalId [弹窗ID]
              * @param htmlUrl [HTML路径]
              * @param id      [更新项ID]
              * @param event   [触发事件]
              */
+            beforeShowModal: function(){},
+            afterShowModal: function(){},
             showModal: function(modalId,htmlUrl,id,event) {
                 var vm = this
-                // 设置查询的ID
-                vm.query.bean = vm.bean
-                vm.query.id = id
+                // =====1,查询初始化操作=====
+                vm.initQuery(id)
 
                 // 设置跳转参数
                 if(event != null){
                     event.target.href = event.target.href.split('#')[0] + '#' + id
                 }
 
-                // =====显示弹窗前置操作=====
+                // =====2,显示弹窗前置操作=====
                 vm.beforeShowModal()
 
                 // 清空其他的弹窗div的内容
@@ -262,7 +268,7 @@ var VueBase = Vue.extend({
                     $('#'+modalId+'Content').find("button:contains('返回')").remove();// 去掉返回按钮
                     $('#'+modalId+'Content' + ' .sub-header').remove();// 去掉标题
 
-                    // =====显示弹窗后置操作=====
+                    // =====4,显示弹窗后置操作=====
                     vm.afterShowModal()
 
                     // 显示详情弹出框
